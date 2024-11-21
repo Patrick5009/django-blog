f7e594162bb9dc68ccf8efaf44c4d930962e77fd
Patrick Walsh <paddybob39@gmail.com>
2024-11-21 00:00:43 +0000

    added about app page, urls.py,about.html and connected it up

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!touch
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-patrick5009-djangoblog-8ig490i8wjk.ws.codeinstitute-ide.net', 
 '.herokuapp.com']
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
     
 ]
 
